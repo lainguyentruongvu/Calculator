a9d4bc08a5475cde80f019d4e98b4ba0322ab46a
lainguyentruongvu <truongvuback1t@gmail.com>
2024-06-11 22:40:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+
+
+const display = document.getElementById('display');
+
+
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearCalculator() {
+    display.value = "";
+}
+function calculator() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "error";
+    }
+}
+
+// let runningTotal = 0;
+// let buffer = "0";
+// let previousOperator;
+
+// const screen = document.querySelector('.screen');
+
+// function buttonClick(value) {
+//     if (isNaN(value)) {
+//         handleSymbol(value);
+//     } else {
+//         handleNumber(value);
+//     }
+//     screen.innerText = buffer;
+
+// }
+
+// function handleSymbol(symbol) {
+//     switch (symbol) {
+//         case 'C':
+//             buffer = '0';
+//             runningTotal = '0';
+//             break;
+//         case '=':
+//             if (previousOperator === null) {
+//                 return
+//             }
+//             flushOperator(parseInt(buffer));
+//             previousOperator = null;
+//             buffer = runningTotal;
+//             runningTotal = 0;
+//             break;
+//         case '←':
+//             if (buffer.length === 1) {
+//                 buffer = '0';
+
+//             } else {
+//                 buffer = buffer.toString(0, buffer.length - 1);
+
+//             }
+//             break;
+//         case '+':
+//         case '-':
+//         case '×':
+//         case '÷':
+//             handleMath(symbol);
+//             break;
+//     }
+// }
+// function handleMath(symbol) {
+//     if (buffer === '0') {
+//         return
+//     }
+//     const intBuffer = parseInt(buffer);
+//     if (runningTotal === 0) {
+//         runningTotal = intBuffer;
+//     } else {
+//         flushOperator(intBuffer);
+//     }
+//     previousOperator = symbol;
+//     buffer = '0';
+// }
+// function flushOperator(intBuffer) {
+//     intBuffer = "0";
+//     console.log("inbut", intBuffer);
+//     if (previousOperator === '+') {
+//         runningTotal = intBuffer + intBuffer;
+//     } else if (previousOperator === '-') {
+//         runningTotal -= intBuffer;
+//     } else if (previousOperator === '×') {
+//         runningTotal *= intBuffer;
+//     } else if (previousOperator === '÷') {
+//         runningTotal /= intBuffer;
+//     }
+// }
+// function handleNumber(numberString) {
+//     if (buffer === "0") {
+//         buffer = numberString;
+//     } else {
+//         buffer += numberString;
+//     }
+// }
+
+// function init() {
+//     document.querySelector('.calc-button').
+//         addEventListener('click', function (event) {
+//             buttonClick(event.target.innerText);
+//         })
+// }
+// init();
